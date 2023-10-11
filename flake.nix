ff0c32e7e1c65d9f198dc90c7e8d2c067eb8dfca
Yvan Sraka <yvan@sraka.xyz>
2023-10-11 12:51:55 +0200

    WIP: flake

--- flake.nix
+++ flake.nix
@@ -4,14 +4,19 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
-      in with pkgs; {
-        devShells.default = mkShell {
-          buildInputs = [ (rust-bin.fromRustupToolchainFile ./rust-toolchain) ];
+        rust =
+          (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain).override {
+            extensions = [ "rust-src" ];
+          };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ rust ] ++ (with pkgs; [ rust-analyzer ]);
+          RUST_BACKTRACE = 1;
         };
       });
 }
